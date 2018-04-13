aaddeed35238054e4614feaf76d293f2cc94e874
BruceWei <brucelin@techbangdeMac-mini.local>
2018-04-13 13:03:15 +0800

    refactor code

--- delete_staging_user.sh
+++ delete_staging_user.sh
@@ -1,47 +1,14 @@
-echo; echo "====Will start deleting user===="; echo
+source ./function.sh
 
-function confirm_email_format(){
-  echo; echo "====please enter user email===="; echo
-  read -p "Please input email: " email
-  if [ `echo $email |grep "^[a-zA-Z0-9_-]*@[A-Za-z_-]*\.[a-zA-Z_-]*$"` ];then
-      echo $email >> result.txt
-  else
-    confirm_email_format
-  fi
-}
+echo; echo "====Will start deleting staging user===="; echo
 
 confirm_email_format
+enter_login
+check_continue "staging"
 
-echo; echo "please enter user login:"; echo
-read -p "Please input login: " login
-
-echo; echo "do you want to delete user on staging?"; echo
-read -r -p "Are You Sure? [Y/n] " input
-
-case $input in
-  [yY][eE][sS]|[yY])
-  echo; echo "====continue to execute===="; echo
-  ;;*)
-  echo; echo "====stop===="; echo
-  exit;;
-esac
-
-echo; echo '====start delete user===='; echo
-
-echo; echo '====delete autozone user===='; echo
-ssh web12 "cd autozone/current && RAILS_ENV=staging /usr/local/rbenv/shims/bundle exec rake 'user:destroy_user_account[$email, $login]'"
-
-echo; echo '====delete manfashion user===='; echo
-# ssh web12 "cd manfashion/current && RAILS_ENV=staging /usr/local/rbenv/shims/bundle exec rake 'user:destroy_user_account[$email, $login]'"
-
-# echo; echo '====delete digiphoto user===='; echo
-# ssh web12 "cd digiphoto/current && RAILS_ENV=staging /usr/local/rbenv/shims/bundle exec rake 'user:destroy_user_account[$email, $login]'"
-
-# echo; echo '====delete t17 user===='; echo
-# ssh web12 "cd forum/current && RAILS_ENV=staging /usr/local/rbenv/shims/bundle exec rake 'user:destroy_user_account[$email, $login]'"
-
-# echo; echo '====delete techbang user===='; echo
-# ssh web12 "cd techbang/current && RAILS_ENV=staging /usr/local/rbenv/shims/bundle exec rake 'user:destroy_user_account[$email, $login]'"
-
-# echo; echo '====delete i user===='; echo
-# ssh web13 "cd techbang-profile/current && RAILS_ENV=staging /usr/local/rbenv/shims/bundle exec rake 'user:destroy_user_account[$email, $login]'"
+delete_user web12 "autozone" "staging"
+delete_user web12 "manfashion" "staging"
+delete_user web12 "digiphoto" "staging"
+delete_user web12 "forum" "staging"
+delete_user web12 "techbang" "staging"
+delete_user web13 "techbang-profile" "staging"
---
+++ function.sh
@@ -0,0 +1,30 @@
+function confirm_email_format(){
+  read -p "Please input email: " email
+  if [ `echo $email |grep "^[a-zA-Z0-9_-]*[+-9]*@[A-Za-z_-]*\.[a-zA-Z_-]*$"` ];then
+      echo $email >> result.txt
+  else
+    confirm_email_format
+  fi
+}
+
+function enter_login() {
+  read -p "Please input login: " login
+}
+
+function check_continue() {
+  echo; echo "do you want to delete user on $1?"; echo
+  read -r -p "Are You Sure? [Y/n] " input
+
+  case $input in
+    [yY][eE][sS]|[yY])
+    echo; echo "====start delete user===="; echo
+    ;;*)
+    echo; echo "====stop===="; echo
+    exit;;
+  esac
+}
+
+function delete_user(){
+  echo; echo "====delete $2 user===="; echo
+  ssh $1 "cd $2/current && RAILS_ENV=$3 /usr/local/rbenv/shims/bundle exec rake 'user:destroy_user_account[$email, $login]'"
+}
