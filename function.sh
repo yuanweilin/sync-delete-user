bbbb9ecc61b4fca46c15dd7d3551cc975f47fa09
shangwei <shangweilin7@gmail.com>
2018-05-28 17:29:58 +0800

    adjust regular expression for email

--- function.sh
+++ function.sh
@@ -1,7 +1,7 @@
 function confirm_email_format(){
   read -p "Please input email: " email
-  if [ `echo $email |grep "^[a-zA-Z0-9_-]*[+-9]*@[A-Za-z_-]*\.[a-zA-Z_-]*.*$"` ];then
-      echo $email >> result.txt
+  if [ `echo $email | grep "^[A-Za-z0-9_-]*[+-9]*@[A-Za-z_-]*\.[A-Za-z_-]*.[A-Za-z_-]*"` ];then
+    echo $email >> result.txt
   else
     confirm_email_format
   fi
